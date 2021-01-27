e634c4f90aeedc141179f467c327dc19a941b0e9
Jordan Masone <jordanmasone1@gmail.com>
2021-01-26 20:55:05 -0500

    Get requests working and POST request.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Require necessary modules
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes")
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
 const path = require("path");
 const fs = require("fs");
 
@@ -13,8 +13,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// app.use("/api", apiRoutes);
+// app.use("*", htmlRoutes);
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.post("/api/notes", function (req, res) {
+    res.json(fs.writeFile("./db/db.json", `[ ${JSON.stringify(req.body)} ]`, 'utf-8', function (err) {
+        if (err) throw err;
+        console.log(req.body);
+        console.log("Note Saved!");
+    }));
+});
 
 // Start the server on the dynamic port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}...`));
+
+// [{"title":"Test Title","text":"Test text"}]
