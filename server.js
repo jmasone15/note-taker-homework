94fe7b05d8082d79e6f1fc23f50b9f9a2b33686b
Jordan Masone <jordanmasone1@gmail.com>
2021-01-26 21:49:13 -0500

    Added uuid to generate random ids.

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require("express");
 // const htmlRoutes = require("./routes/htmlRoutes");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 //Initialize the app and create a dynamic port for heroku
 const app = express();
 const PORT = process.env.PORT || 3000;
+const db = "./db/db.json"
 
 // Set up body parsing, static, and route middleware
 app.use(express.urlencoded({ extended: true }));
@@ -20,31 +22,50 @@ app.use(express.static("public"));
 // require("./routes/htmlRoutes")(app);
 
 app.get("/", function (req, res) {
+
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
+
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+
+    fs.readFile(path.join(__dirname, db), function (err, data) {
+        if (err) throw err;
+        return res.json(JSON.parse(data));
+    });
+
 });
 
 app.post("/api/notes", function (req, res) {
-    res.json(fs.writeFile("./db/db.json", `[ ${JSON.stringify(req.body)} ]`, 'utf-8', function (err) {
-        if (err) throw err;
-        console.log(req.body);
-        console.log("Note Saved!");
-    }));
-});
 
-app.delete("/api/notes/:id", function (req, res) {
-    res.json(fs.writeFile("./db/db.json", JSON.stringify(req.body), 'utf-8', function (err) {
+    fs.readFile(path.join(__dirname, db), function (err, data) {
         if (err) throw err;
-        console.log(req.body);
-        console.log("Note Deleted!");
-    }));
+        let userInput = JSON.parse(data);
+        userInput.push(req.body)
+        req.body.id = uuidv4();
+
+
+        res.json(fs.writeFile(db, JSON.stringify(userInput), function (err) {
+            if (err) throw err;
+            console.log(userInput);
+            console.log("Note Created!");
+        }));
+
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+
+        return res.json(fs.writeFile(db, JSON.stringify(req.body), 'utf-8', function (err) {
+            if (err) throw err;
+            console.log(req.body);
+            console.log("Note Deleted!");
+        }));
+
+    });
 });
 
 // Start the server on the dynamic port
